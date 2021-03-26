556ded7472dbcaa61f9e37defafeb92d9ef448c1
Dillon O'Connor <dilong818@gmail.com>
2021-03-26 14:58:42 -0400

    set up basic flow with rooms controller and room channel subscription creation

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,16 +1,22 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("RoomChannel", {
-  connected() {
-    console.log("Connected to channel.");
-    // Called when the subscription is ready for use on the server
-  },
-
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
-
-  received(data) {
-    console.log(data);
+document.addEventListener("turbolinks:load", () => {
+  if (document.querySelector("#room-id")) {
+    const roomHeader = document.getElementById("room-id");
+    const roomData = roomHeader.getAttribute("data-room-id");
+    
+    consumer.subscriptions.create({channel: "RoomChannel", room_id: roomData}, {
+      connected() {
+        console.log(`Connected to room channel ${roomData}.`);
+      },
+    
+      disconnected() {
+      },
+    
+      received(data) {
+      }
+    });
   }
 });
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+NOUNS = ["Astronauts", "Players", "Gamers", "Ballers", "Professionals", "Workers", "Weavers", "Artists", "Animals"]
+ADJECTIVES = ["Huge", "Amazing", "Exceptional", "Lazy", "Wondrous", "Mysterious", "Magical", "Serious", "Somber"]
+
+10.times { |i| Room.create!(name: "#{ADJECTIVES.sample} #{NOUNS.sample} - #{i}") }
+
